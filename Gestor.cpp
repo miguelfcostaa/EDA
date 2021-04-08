214e77a31675fbfbb310789e2fdec47dd33cbfd5
Itsme_Ines <61025645+2105319@users.noreply.github.com>
2021-04-08 13:56:09 +0100

    Create Gestor.cpp

---
+++ Gestor.cpp
@@ -0,0 +1,58 @@
+void Gestor() {
+	bool sair = false;
+	char opcao;
+	do
+	{
+		cout << "***** Bem Vindo Gestor *****" << endl;;
+		cout << "(1) - Remover Produtos" << endl;
+		cout << "(2) - Atualizar Preço" << endl;
+		cout << "(3) - Iniciar Campanha" << endl;
+		cout << "(4) - Gravar Supermercado" << endl;
+		cout << "(5) - Carregar Supermercado" << endl;
+		cout << "(6) - Imprimir Produtos" << endl;
+		cout << "(7) - Criar nova área" << endl;
+		cout << "(8) - Mostrar registo de vendas" << endl;
+		cout << "(9) - Alterar área" << endl;
+		cout << "(0) - Voltar" << endl;
+		cout << "Selecione a sua opção:" << endl;
+		cin >> opcao;
+		cout << endl;
+		switch (opcao) {
+		case '1':
+			
+			break;
+		case '2':
+			
+			break;
+		case '3':
+			
+			break;
+		case '4':
+			
+			break;
+		case '5':
+			
+			break;
+		case '6':
+			cout << "Posicao Valor Minimo " << posicaoValorMin(v, tamanho) << endl;
+			break;
+		case '7':
+			
+			break;
+		case '8':
+
+			break;
+		case '9':
+
+			break;
+		case '0':
+			cout << "Escolheu a opcao Sair. Adeus!" << endl;
+			sair = true;
+			break;
+		default:
+			cout << "Escolha uma opcao valida." << endl;
+		}
+	} while (!sair);
+
+}
+
