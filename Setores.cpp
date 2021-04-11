ea241a5ae764c9ea48ab188b3010c84f1f75fddd
Itsme_Ines <61025645+2105319@users.noreply.github.com>
2021-04-11 16:30:01 +0100

    Update Setores.cpp

--- Setores.cpp
+++ Setores.cpp
@@ -3,3 +3,9 @@
 #include <time.h>
 
 using namespace std;
+
+
+
+cout << "Sector : A  |  Responsavel : " << nomes << "  |  Capacidade : " << capacidade << "  |  Produto : " << produto << "  |  Área : " << area << endl;
+cout << "Produto : " << produto << "  |  Preço : " << endl;
+
