e8e7816b8cd1df41c1dd686eb2a5754c87702756
Itsme_Ines <61025645+2105319@users.noreply.github.com>
2021-04-11 18:40:55 +0100

    Update Setores.cpp

--- Setores.cpp
+++ Setores.cpp
@@ -1,13 +1,50 @@
-#include <iostream>
 #include <stdlib.h>
-#include <stdio.h>
+#include<iostream>
 #include <time.h>
-#include <locale.h>
+#include <fstream>
 
 using namespace std;
 
 
+int setor() {
 
-cout << "Sector : A  |  Responsavel : " << nomes << "  |  Capacidade : " << capacidade << "  |  Produto : " << produto << "  |  Área : " << area << endl;
-cout << "Produto : " << produto << "  |  Preço : " << endl;
+	cout << "Sector : " << letra << "  |  Responsavel : " << nome << "  |  Capacidade : " << capacidade << "  |  Produto : " << produto << "  |  Área : " << area << endl;
+	cout << "Produto : " << produto << "  |  Preço : " << endl;
 
+}
+
+
+int nome(int nome) {
+	cout << "Insira o nome do responsavel do sector " << letra << " : ";
+	cin >> nome;
+	cout << endl;
+	return nome;
+}
+
+void capacidade(int capacidade) {
+	capacidade = rand() % 10 + 5;
+}
+
+string produto() {
+	ifstream produto("nome.txt");
+	string line;
+	while (produto.good()) {
+		getline(produto, line);
+		line = produto.get();
+		cout << line;
+	}
+	produto.close();
+	return line;
+}
+
+string area() {
+	ifstream area("areas.txt");
+	string line;
+	while (area.good()) {
+		getline(area, line);
+		line = area.get();
+		cout << line;
+	}
+	area.close();
+	return line;
+}
