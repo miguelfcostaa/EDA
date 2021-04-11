3e1caf9916f509cf2f86b5eb807ae441c83d377d
Itsme_Ines <61025645+2105319@users.noreply.github.com>
2021-04-11 17:25:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-#include <stdlib.h>
+#include < stdlib.h >
 #include <time.h>
 
 using namespace std;
 
 int main() {
+	int escolha = -1;
+	cout << "(s) Seguinte   |   (g)Gestão   |   (n)Sair" << endl;
+	cin >> escolha;
+	if (escolha == 's') {
 
+	}
+	else if (escolha == 'g') {
+
+	}
+	else if (escolha == 'n') {
+
+	}
 }
