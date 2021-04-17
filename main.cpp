88f9a7301b9859c3f7c3b74ccab2ff20c8fe2838
Itsme_Ines <61025645+2105319@users.noreply.github.com>
2021-04-17 14:37:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,17 +11,27 @@ using namespace std;
 
 int main() {
 	locale::global(locale(""));
-	int escolha = -1;
-	cout << "(s) Seguinte   |   (g)Gestão   |   (n)Sair" << endl;
-	cin >> escolha;
-	if (escolha == 's') {
-		sector();
-	}
-	else if (escolha == 'g') {
-		Gestor();
-	}
-	else if (escolha == 'n') {
-
-	}
+	char escolha;
+	int Nsector = rand() % 12 + 8;
+	bool sair = false;
+	do {
+		cout << "(s) Seguinte   |   (g)Gestão   |   (n)Sair" << endl;
+		cin >> escolha;
+		switch (escolha) {
+		case 's':
+			sector(Nsector);
+			break;
+		case 'g':
+			gestor();
+			break;
+		case 'n':
+			sair = true;
+			break;
+		default:
+			cout << "Escolha uma opçâo válida" << endl;
+		}
+	} while (!sair);
 	return 0;
 }
+
+
