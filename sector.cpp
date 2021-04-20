eafeb2ec9bc0f54ec8483455098651a18cc37a2c
Itsme_Ines <61025645+2105319@users.noreply.github.com>
2021-04-20 14:33:20 +0100

    Update sector.cpp

--- sector.cpp
+++ sector.cpp
@@ -7,25 +7,25 @@
 
 using namespace std;
 
-int nomes() {
-	int i;
-	sector* sectores;
-	int nome;
+//Dar o nome do responsavel
+string* nomes(sector* sectores, int i) {
+	string* nome = new string[30];
 	cout << "Insira o nome do responsavel do sector " << sectores[i].letra << " : ";
-	cin >> nome;
-	cout << endl;
 	return nome;
 }
 
+//Dar a capacidade do sector
 int capacidade() {
 	int Ncapacidade = rand() % 10 + 5;
 	return Ncapacidade;
 }
 
-int Nprodutos(){
+
+int Nprodutos() {
 	return 0;
 };
 
+//Ler a lista dos produtos
 string* produtos() {
 	ifstream produto("nome.txt");
 	string l;
@@ -41,7 +41,7 @@ string* produtos() {
 	return line;
 }
 
-
+//Ler a lista das areas
 string* areas() {
 	ifstream area("areas.txt");
 	string l;
@@ -57,21 +57,26 @@ string* areas() {
 	return line;
 }
 
+//Funcao para criar os sectores
 sector* criasector(sector* sectores, int Nsector) {
-	char* alfa = new char[26] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-	string* line = produtos();
-	string* lines = areas();
+	char* letra = new char[26]{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 	for (int i = 0; i < Nsector; i++) {
-		sectores[i].letra = alfa[i];
-		sectores[i].nome = nomes();
+		sectores[i].letra = letra[i];
+		string* name = nomes(sectores, i);
+		sectores[i].nome = name[i];
 		sectores[i].capacidade = capacidade();
 		sectores[i].Nprodutos = Nprodutos();
+		string* line = produtos();
 		sectores[i].produtos = line[i];
+		string* lines = areas();
 		sectores[i].areas = lines[i];
+		cin.ignore();
+		cin.get();
 	}
 	return sectores;
 }
 
+//Funcao para mostrar os sectores
 void mostraSector(sector* sectores, int Nsector) {
 	for (int i = 0; i < Nsector; i++) {
 		cout << "Sector : " << sectores[i].letra << "  |  Responsavel : " << sectores[i].nome << "  |  Capacidade : " << sectores[i].capacidade << "  |  Ocupação : " << sectores[i].Nprodutos << "  |  Área : " << sectores[i].areas << endl;
