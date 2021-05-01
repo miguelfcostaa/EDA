842fc1a8f8f5ed34a6db1662d034c4cf185af7ca
Miguel Costa <82154127+costaa88@users.noreply.github.com>
2021-05-01 20:22:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,31 +11,32 @@
 using namespace std;
 
 int main() {
-	locale::global(locale());
-	srand(time(NULL));
-	int Nsector = rand() % 5 + 8;	//numero random de 8 a 12
-	cout << Nsector << endl;
-	sector* sectores = new sector[Nsector];	//criar object para a struct
-	criasector(sectores, Nsector);	//criar os sectores antes de mostrar
-	char escolha;
-	bool sair = false;
-	do {
-		cout << "(s) Seguinte   |   (g)Gestão   |   (n)Sair" << endl;
-		cin >> escolha;
-		switch (escolha) {
-		case 's':
-			mostraSector(sectores, Nsector);
-			break;
-		case 'g':
-			gestor();
-			break;
-		case 'n':
-			sair = true;
-			break;
-		default:
-			cout << "Escolha uma opçâo válida" << endl;
-			break;
-		}
-	} while (!sair);
-	return 0;
+    locale::global(locale(""));
+    srand(time(NULL));
+    int Nsector = rand() % 5 + 8;    //numero random de 8 a 12
+    sector* sectores = new sector[Nsector];    //criar object para a struct
+    prod* produt = new prod;
+    criasector(sectores, Nsector);    //criar os sectores antes de mostrar
+    criaProd(sectores, produt, Nsector);
+    char escolha;
+    bool sair = false;
+    do {
+        cout << "(s) Seguinte   |   (g)Gestão   |   (n)Sair" << endl;
+        cin >> escolha;
+        switch (escolha) {
+        case 's':
+            mostraSector(sectores, produt, Nsector);
+            break;
+        case 'g':
+            gestor();
+            break;
+        case 'n':
+            sair = true;
+            break;
+        default:
+            cout << "Escolha uma opção válida" << endl;
+            break;
+        }
+    } while (!sair);
+    return 0;
 }
