1615c6290a575a567af8ca71286c5b4461788c79
Miguel Costa <82154127+costaa88@users.noreply.github.com>
2021-05-01 20:21:45 +0100

    Update sector.h

--- sector.h
+++ sector.h
@@ -4,13 +4,22 @@
 using namespace std;
 
 struct sector {
-	char letra;
-	string nome;
-	int capacidade;
-	int Nprodutos;
-	string produtos;
-	string areas;
+    char letra;
+    string nome;
+    int capacidade;
+    int Nproduto;
+    string areas;
+    struct prod;
 };
 
+struct prod {
+    double preco;
+    string produto;
+};
+
+prod* criaProd(sector* sectores, prod* produt, int Nsector);
+//int prec(prod* produt);
+int prec(sector* sectores, prod* produt, int Nsector);
+
 sector* criasector(sector* sectores, int Nsector);
-void mostraSector(sector* sectores, int Nsector);
+void mostraSector(sector* sectores, prod* produt, int Nsector);
