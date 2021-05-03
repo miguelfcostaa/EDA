10420bae42e09c30b54f03dcd630528bf2aac65f
Miguel Costa <82154127+costaa88@users.noreply.github.com>
2021-05-03 22:20:52 +0100

    Update armazem.h

--- armazem.h
+++ armazem.h
@@ -4,8 +4,10 @@
 using namespace std;
 
 struct armazem {
-	string armazem_produtos;
-	int n_produtos;
+	string armazem_produtos = "Vazio";
+	int n_produtos = 0;
 };
 
 armazem* criaArmazem(armazem* ap, prod* produt);
+string* stock();
+void mostraArmazem(armazem* ap, prod* produt);
