ea62952e2c28ccd4af1f588b59761cf1dfaf0ece
Miguel Costa <82154127+costaa88@users.noreply.github.com>
2021-05-03 18:42:23 +0100

    Add files via upload

---
+++ armazem.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "sector.h"
+#include <string>
+#include <fstream>
+#include "armazem.h"
+
+using namespace std;
+
+/*
+string* produtos() {
+	ifstream produto("nome.txt");
+	string l;
+	string* lines = new string[24];
+	int i = 0;
+	if (produto.is_open()) {
+		while (getline(produto, l)) {
+			lines[i] = l;
+			i++;
+		}
+		produto.close();
+		cin.ignore();
+	}
+	return lines;
+}
+
+
+int criaArmazem(armazem* ap, prod* produt) {
+	string* lines = produtos();
+	for (int i = 0; i < 50; i++) {
+		ap[i].armazem_produtos = lines[i];
+	}
+	return 0;
+}
+
+
+void addProdutos(armazem* ap) {
+	prod* produt = criaProd(5);
+	armazem->stock = prod;
+}
+*/
