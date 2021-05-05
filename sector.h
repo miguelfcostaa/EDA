7313f616c3659f58e1e635be53c300ac75ff3b9c
Itsme_Ines <61025645+2105319@users.noreply.github.com>
2021-05-05 19:46:03 +0100

    Update sector.h

--- sector.h
+++ sector.h
@@ -3,45 +3,29 @@
 
 using namespace std;
 
-/*
-struct sector {
-    struct prod {
-        prod* preco = 0;
-        string produto = "Vazio";
-        string fornecedores;
-    };
-    char letra = ' ';
-    string areas;
-    string nome;
-    int capacidade = 0;
-    int Nproduto = 0;
-};
-
-sector::prod* novo = new sector::prod();
-*/
 
 struct sector {
-    char letra = ' ';
+    char letra;
     string areas;
     string nome;
-    int capacidade = 0;
-    int Nproduto = 0;
+    int capacidade;
+    int Nproduto;
 };
 
 struct prod {
-    int preco = 0;
-    string produto = "Vazio";
+    int preco ;
+    string produto ;
     string fornecedores;
 };
 
-
-int fullcapacidade(sector* sectores, int Nsector);
 string* produtos();
 string* nomes(sector* sectores, int i);
-int capacidade();
 int Nproduto(sector* sectores, int Nsector);
+int capacidade();
 string* areas();
-sector* criasector(sector* sectores, int Nsector);
-prod* criaProd(sector* sectores, prod* produt, int Nsector);
+int preco();
+int fullcap(sector* sectores, int Nsector);
 
+sector* criasector(sector* sectores, int Nsector);
+prod* criaProd(prod* produt, sector* sectores, int Nsector);
 void mostraSector(sector* sectores, prod* produt, int Nsector);
