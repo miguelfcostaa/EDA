017dca5093a005e9138a3bc915b1326ecbf4957a
Kawaii <82154502+KikaKawaii@users.noreply.github.com>
2021-05-06 22:44:40 +0100

    Update Gestor.h

--- Gestor.h
+++ Gestor.h
@@ -2,7 +2,6 @@
 #include "sector.h"
 #include "armazem.h"
 
-
 void removeProd(prod* produt, armazem* ap);
 int atualizaPreco(sector* sectores, int Nsector, prod* produt, armazem* ap);
 void iniciarCampanha(int Nsector, sector* sectores, prod* produt);
