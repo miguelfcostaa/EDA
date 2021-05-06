8d92c1237c1b03d10a76f386c6226f591d0ac43f
Kawaii <82154502+KikaKawaii@users.noreply.github.com>
2021-05-06 22:42:55 +0100

    Update sector.h

--- sector.h
+++ sector.h
@@ -3,8 +3,14 @@
 
 using namespace std;
 
+struct prod {
+    int preco;
+    string produto;
+    string fornecedores;
+};
 
 struct sector {
+    prod* prods;
     char letra;
     string areas;
     string nome;
@@ -12,11 +18,13 @@ struct sector {
     int Nproduto;
 };
 
-struct prod {
-    int preco ;
-    string produto ;
-    string fornecedores;
+struct armazem {
+    string armazem_produtos;
+    int n_produtos;
+    int preco;
 };
+
+
 
 string* produtos();
 string* nomes(sector* sectores, int i);
@@ -24,8 +32,8 @@ int Nproduto(sector* sectores, int Nsector);
 int capacidade();
 string* areas();
 int preco();
-int fullcap(sector* sectores, int Nsector);
+//int fullcap(sector* sectores, int Nsector);
 
-sector* criasector(sector* sectores, int Nsector);
-prod* criaProd(prod* produt, sector* sectores, int Nsector);
+sector* criasector(sector* sectores, int Nsector, prod* produt, armazem* ap);
+//prod* criaProd(sector*sectores,prod*produt);
 void mostraSector(sector* sectores, prod* produt, int Nsector);
