ab14d35c7b62475ce3b9f0577cba05602be02f21
Miguel Costa <82154127+costaa88@users.noreply.github.com>
2021-05-29 13:49:18 +0100

    Update Gestor.h

--- Gestor.h
+++ Gestor.h
@@ -7,12 +7,10 @@ void removeProd(armazem* ap, int Nsector, sector* sectores);
 armazem* atualizaPreco(sector* sectores, int Nsector, prod* produt, armazem* ap);
 void iniciarCampanha(int Nsector, sector* sectores, prod* produt);
 void ordemAlfabetica(sector* sectores, int Nsector, prod* produt, armazem* ap);
-//void gravarSuper();
+void gravarsuper(string gravac, sector* sectores, int Nsector, prod* produt);
 //void carregaSuper();
 void imprimeProdutos(sector* sectores, int Nsector, prod* produt, armazem* ap);
 //void novaArea();
-//void MostraReg();
-//void alteraArea();
-//void voltar();
-
+void mostraregisto(sector* sectores, int Nsector);
+void alteraArea(armazem* ap, sector* sectores, int Nsector);
 void gestor(sector* sectores, int Nsector, prod* produt, armazem* ap);
