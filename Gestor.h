37d9d88f45757480f292b865385226742bbc30ff
Miguel Costa <82154127+costaa88@users.noreply.github.com>
2021-06-07 22:50:20 +0100

    Update Gestor.h

--- Gestor.h
+++ Gestor.h
@@ -10,7 +10,6 @@ void ordemAlfabetica(sector* sectores, int Nsector, prod* produt, armazem* ap);
 void gravarsuper(string gravac, sector* sectores, int Nsector, prod* produt);
 //void carregaSuper();
 void imprimeProdutos(sector* sectores, int Nsector, prod* produt, armazem* ap);
-//void novaArea();
 void mostraregisto(sector* sectores, int Nsector);
 void alteraArea(armazem* ap, sector* sectores, int Nsector);
 void gestor(sector* sectores, int Nsector, prod* produt, armazem* ap);
